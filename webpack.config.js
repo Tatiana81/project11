38c26749b100e14ca05e4b63ad0ce38907e81aaf
Tatiana <kriista@mail.ru>
2020-03-22 02:12:50 +0300

    css compile

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const MIniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebpackMd5Hash = require('webpack-md5-hash')
+
+module.exports = {
+    entry: { main: "./src/js/index.js" },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[chunkhash].js'
+    },
+    module: {
+        rules: [{
+                test: /\.js$/,
+                use: { loader: "babel-loader" },
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [MIniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+            }
+        ]
+    },
+    plugins: [
+        new MIniCssExtractPlugin({ filename: 'index.[contenthash].css' }),
+        new HtmlWebpackPlugin({
+            // Означает, что:
+            inject: false, // стили НЕ нужно прописывать внутри тегов
+            template: './src/index.html', // откуда брать образец для сравнения с текущим видом проекта
+            filename: 'index.html' // имя выходного файла, то есть того, что окажется в папке dist после сборки
+        })
+    ]
+}
